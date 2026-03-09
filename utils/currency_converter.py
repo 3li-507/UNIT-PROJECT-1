a8c55bc5b92e32f8dc34198ba943dd64e81d92c6
Ali Alshehri <ps4ps5cc@gmail>
2026-03-09 10:24:46 +0300

    Add currency_converter method to convert between USD and SAR

---
+++ utils/currency_converter.py
@@ -0,0 +1,9 @@
+# utils/currency_converter.py
+def convert_amount(amount, from_currency, to_currency):
+    """Convert amount between SAR and USD"""
+    # 1 USD = 3.75 SAR 
+    if from_currency == "SAR" and to_currency == "USD":
+        return amount / 3.75
+    elif from_currency == "USD" and to_currency == "SAR":
+        return amount * 3.75
+    return amount  
